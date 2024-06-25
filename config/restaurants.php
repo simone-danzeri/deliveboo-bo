3da660401ed25ad40933bc569341fdb403bdfb89
simone-danzeri <s.danzeri@icloud.com>
2024-06-25 18:22:57 +0200

    feat: added 4 more (5 total) new restaurants

--- config/restaurants.php
+++ config/restaurants.php
@@ -10,4 +10,44 @@ return [
         "email" => "lapergola@mail.it",
         "vat_number" => "89457634764"
     ],
+    [
+        "restaurant_name" => "La Luna Del Porro",
+        "slug" => "la-luna-del-porro",
+        "user_id" => "2",
+        "address" => "Viale dei Gigli, 6, Rovello Porro (MI)",
+        "phone" => "3898538641",
+        "img" => "",
+        "email" => "lalunadelporro@mail.it",
+        "vat_number" => "74629649052"
+    ],
+    [
+        "restaurant_name" => "Da Luciano",
+        "slug" => "da-luciano",
+        "user_id" => "3",
+        "address" => "Corso dei Gelsomini, 27, Rovello Porro (MI)",
+        "phone" => "3459863411",
+        "img" => "",
+        "email" => "daluciano@mail.it",
+        "vat_number" => "63626096437"
+    ],
+    [
+        "restaurant_name" => "Il Milione",
+        "slug" => "il-milione",
+        "user_id" => "4",
+        "address" => "Via dei Narcisi, 23, Rovello Porro (MI)",
+        "phone" => "3765419840",
+        "img" => "",
+        "email" => "ilmilione@mail.it",
+        "vat_number" => "63927407372"
+    ],
+    [
+        "restaurant_name" => "Il Giardino Di Giada",
+        "slug" => "il-giardino-di-giada",
+        "user_id" => "5",
+        "address" => "Viale delle Betulle, 8, Rovello Porro (MI)",
+        "phone" => "3412643895",
+        "img" => "",
+        "email" => "ilgiardinodigiada@mail.it",
+        "vat_number" => "46703526973"
+    ],
 ];
