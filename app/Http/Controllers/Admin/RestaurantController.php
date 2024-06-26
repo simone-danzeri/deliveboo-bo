35835e3e4e37e876d4016a7369af5144538d47b4
BruneiPaulPotosiCcuno <neoxtrotoka@gmail.com>
2024-06-26 23:28:36 +0200

    add-show

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -54,9 +54,9 @@ class RestaurantController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
-    {
-        //
+    public function show(Restaurant $restaurant)
+    {   
+        return view('admin.projects.show', compact('restaurant'));
     }
 
     /**
