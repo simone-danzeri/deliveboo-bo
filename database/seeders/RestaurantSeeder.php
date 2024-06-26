dee947cce3cd2bccf8fc71b5f016ab92865c1bbe
simone-danzeri <s.danzeri@icloud.com>
2024-06-26 15:11:12 +0200

    fix: added seeder for restaurant

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Restaurant;
 
 class RestaurantSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class RestaurantSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $restaurantArray = config('restaurants');
+        foreach($restaurantArray as $eachRestaurant) {
+            $newRestaurant = new Restaurant();
+            $newRestaurant->restaurant_name = $eachRestaurant['restaurant_name'];
+            $newRestaurant->slug = $eachRestaurant['slug'];
+            $newRestaurant->user_id = $eachRestaurant['user_id'];
+            $newRestaurant->address = $eachRestaurant['address'];
+            $newRestaurant->phone = $eachRestaurant['phone'];
+            $newRestaurant->phone = $eachRestaurant['phone'];
+            $newRestaurant->img = $eachRestaurant['img'];
+            $newRestaurant->email = $eachRestaurant['email'];
+            $newRestaurant->vat_number = $eachRestaurant['vat_number'];
+            // $newRestaurant->save();
+        }
     }
 }
