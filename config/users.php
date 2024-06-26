9bd2648eca883686762e762f1fbb66ed85d2b893
simone-danzeri <s.danzeri@icloud.com>
2024-06-26 11:38:22 +0200

    feat: added more users

--- config/users.php
+++ config/users.php
@@ -5,4 +5,24 @@ return [
         'email'    => 'luciano@mail.it',
         'password' => 'password',
     ],
+    [
+        'name'     => 'Marco Rossi',
+        'email'    => 'marco@mail.it',
+        'password' => 'password',
+    ],
+    [
+        'name'     => 'Raimondo Vianello',
+        'email'    => 'raimondo@mail.it',
+        'password' => 'password',
+    ],
+    [
+        'name'     => 'Carlos Flores',
+        'email'    => 'carlos@mail.it',
+        'password' => 'password',
+    ],
+    [
+        'name'     => 'Priscilla Marcantoni Russo',
+        'email'    => 'priscilla@mail.it',
+        'password' => 'password',
+    ],
 ];
