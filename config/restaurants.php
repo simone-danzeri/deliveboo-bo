875c29f0a28d10a25b154f74e8daa3f65c386513
simone-danzeri <s.danzeri@icloud.com>
2024-06-26 10:20:09 +0200

    feat: added restaurants cover images

--- config/restaurants.php
+++ config/restaurants.php
@@ -1,52 +1,57 @@
 <?php
 return [
+    // User 1
     [
         "restaurant_name" => "La Pergola",
         "slug" => "la-pergola",
         "user_id" => "1",
         "address" => "Via dei Fiori, 3, Rovello Porro (MI)",
         "phone" => "3896673429",
-        "img" => "",
+        "img" => "cover-restaurants/2374504731.jpeg",
         "email" => "lapergola@mail.it",
         "vat_number" => "89457634764"
     ],
+    // User 2
     [
         "restaurant_name" => "La Luna Del Porro",
         "slug" => "la-luna-del-porro",
         "user_id" => "2",
         "address" => "Viale dei Gigli, 6, Rovello Porro (MI)",
         "phone" => "3898538641",
-        "img" => "",
+        "img" => "cover-restaurant/3856392841.jpeg",
         "email" => "lalunadelporro@mail.it",
         "vat_number" => "74629649052"
     ],
+    // User 3
     [
         "restaurant_name" => "Da Luciano",
         "slug" => "da-luciano",
         "user_id" => "3",
         "address" => "Corso dei Gelsomini, 27, Rovello Porro (MI)",
         "phone" => "3459863411",
-        "img" => "",
+        "img" => "cover-restaurant/3857305796.jpeg",
         "email" => "daluciano@mail.it",
         "vat_number" => "63626096437"
     ],
+    // User 4
     [
         "restaurant_name" => "Il Milione",
         "slug" => "il-milione",
         "user_id" => "4",
         "address" => "Via dei Narcisi, 23, Rovello Porro (MI)",
         "phone" => "3765419840",
-        "img" => "",
+        "img" => "cover-restaurant/7730472743.jpeg",
         "email" => "ilmilione@mail.it",
         "vat_number" => "63927407372"
     ],
+    // User 5
     [
         "restaurant_name" => "Il Giardino Di Giada",
         "slug" => "il-giardino-di-giada",
         "user_id" => "5",
         "address" => "Viale delle Betulle, 8, Rovello Porro (MI)",
         "phone" => "3412643895",
-        "img" => "",
+        "img" => "cover-restaurant/9473758091.jpeg",
         "email" => "ilgiardinodigiada@mail.it",
         "vat_number" => "46703526973"
     ],
