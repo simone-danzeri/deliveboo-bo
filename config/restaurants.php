365fb6e32aeb116b2b629bf1487e2211bd779288
simone-danzeri <s.danzeri@icloud.com>
2024-06-26 11:22:29 +0200

    fix: changed the links to img for restaurants and gitignore

--- config/restaurants.php
+++ config/restaurants.php
@@ -7,7 +7,7 @@ return [
         "user_id" => "1",
         "address" => "Via dei Fiori, 3, Rovello Porro (MI)",
         "phone" => "3896673429",
-        "img" => "cover-restaurants/2374504731.jpeg",
+        "img" => "https://images.visititaly.eu/uploads/articoli/paragrafo/2024130333-migliori-ristoranti-dove-mangiare-piemonte.jpg",
         "email" => "lapergola@mail.it",
         "vat_number" => "89457634764"
     ],
@@ -18,7 +18,7 @@ return [
         "user_id" => "2",
         "address" => "Viale dei Gigli, 6, Rovello Porro (MI)",
         "phone" => "3898538641",
-        "img" => "cover-restaurant/3856392841.jpeg",
+        "img" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/dc/33/74/photo0jpg.jpg?w=300&h=-1&s=1.jpeg",
         "email" => "lalunadelporro@mail.it",
         "vat_number" => "74629649052"
     ],
@@ -29,7 +29,7 @@ return [
         "user_id" => "3",
         "address" => "Corso dei Gelsomini, 27, Rovello Porro (MI)",
         "phone" => "3459863411",
-        "img" => "cover-restaurant/3857305796.jpeg",
+        "img" => "https://wips.plug.it/cips/paginegialle.it/magazine/cms/2018/10/migliori-ristoranti-roma.jpg?w=744&h=418&a=c",
         "email" => "daluciano@mail.it",
         "vat_number" => "63626096437"
     ],
@@ -40,7 +40,7 @@ return [
         "user_id" => "4",
         "address" => "Via dei Narcisi, 23, Rovello Porro (MI)",
         "phone" => "3765419840",
-        "img" => "cover-restaurant/7730472743.jpeg",
+        "img" => "https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/styles/article_1200_800_fallback/public/fdl_content_import_it/ristoranti-all-aperto-roma.jpg?itok=Yk1o4aDO",
         "email" => "ilmilione@mail.it",
         "vat_number" => "63927407372"
     ],
@@ -52,7 +52,7 @@ return [
         "address" => "Viale delle Betulle, 8, Rovello Porro (MI)",
         "phone" => "3412643895",
         "img" => "cover-restaurant/9473758091.jpeg",
-        "email" => "ilgiardinodigiada@mail.it",
+        "email" => "https://scattidigusto.b-cdn.net/wp-content/uploads/2022/07/Giardino-Eden-Ischia.jpeg",
         "vat_number" => "46703526973"
     ],
 ];
