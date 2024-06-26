ed0e86febda2816aff1ac4010277031b9dbc26fd
gianlucafalaschi <gian100gian@outlook.it>
2024-06-26 15:46:32 +0200

    added restaurants

--- config/restaurants.php
+++ config/restaurants.php
@@ -9,7 +9,19 @@ return [
         "phone" => "3896673429",
         "img" => "https://images.visititaly.eu/uploads/articoli/paragrafo/2024130333-migliori-ristoranti-dove-mangiare-piemonte.jpg",
         "email" => "lapergola@mail.it",
-        "vat_number" => "89457634764"
+        "vat_number" => "89457634764",
+        "type_id" => ""
+    ],
+    [
+        "restaurant_name" => "Sakura",
+        "slug" => "sakura",
+        "user_id" => "1",
+        "address" => "Via dei Ciliegi, 6, Rovello Porro (MI)",
+        "phone" => "3891246975",
+        "img" => "https://m.media-amazon.com/images/I/81vaU1vgk-L._AC_UF1000,1000_QL80_.jpg",
+        "email" => "sakura@mail.it",
+        "vat_number" => "17356835891",
+        "type_id" => ""
     ],
     // User 2
     [
@@ -20,7 +32,19 @@ return [
         "phone" => "3898538641",
         "img" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/16/dc/33/74/photo0jpg.jpg?w=300&h=-1&s=1.jpeg",
         "email" => "lalunadelporro@mail.it",
-        "vat_number" => "74629649052"
+        "vat_number" => "74629649052",
+        "type_id" => ""
+    ],
+    [
+        "restaurant_name" => "Panda Mama",
+        "slug" => "panda-mama",
+        "user_id" => "2",
+        "address" => "Via Marco Polo, 11, Rovello Porro (MI)",
+        "phone" => "3895357890",
+        "img" => "https://i0.wp.com/lilimadeleine.com/wp-content/uploads/2022/02/ronin-milano-ristorante.jpg?fit=1000%2C750&ssl=1",
+        "email" => "sakura@mail.it",
+        "vat_number" => "37356535891",
+        "type_id" => ""
     ],
     // User 3
     [
@@ -31,7 +55,8 @@ return [
         "phone" => "3459863411",
         "img" => "https://wips.plug.it/cips/paginegialle.it/magazine/cms/2018/10/migliori-ristoranti-roma.jpg?w=744&h=418&a=c",
         "email" => "daluciano@mail.it",
-        "vat_number" => "63626096437"
+        "vat_number" => "63626096437",
+        "type_id" => ""
     ],
     // User 4
     [
@@ -42,7 +67,19 @@ return [
         "phone" => "3765419840",
         "img" => "https://www.finedininglovers.it/sites/g/files/xknfdk1106/files/styles/article_1200_800_fallback/public/fdl_content_import_it/ristoranti-all-aperto-roma.jpg?itok=Yk1o4aDO",
         "email" => "ilmilione@mail.it",
-        "vat_number" => "63927407372"
+        "vat_number" => "63927407372",
+        "type_id" => ""
+    ],
+    [
+        "restaurant_name" => "Sushi Love",
+        "slug" => "sushi-love",
+        "user_id" => "4",
+        "address" => "Via Romana, 56, Rovello Porro (MI)",
+        "phone" => "3671257589",
+        "img" => "https://scattidigusto.b-cdn.net/wp-content/uploads/2018/01/Kowa-nuovo-ristorante-giapponese-cinese-Milano-1280x765.jpg",
+        "email" => "sushilove@mail.it",
+        "vat_number" => "84637538692",
+        "type_id" => ""
     ],
     // User 5
     [
@@ -53,6 +90,18 @@ return [
         "phone" => "3412643895",
         "img" => "cover-restaurant/9473758091.jpeg",
         "email" => "https://scattidigusto.b-cdn.net/wp-content/uploads/2022/07/Giardino-Eden-Ischia.jpeg",
-        "vat_number" => "46703526973"
+        "vat_number" => "46703526973",
+        "type_id" => ""
+    ],
+    [
+        "restaurant_name" => "Noodle Star",
+        "slug" => "noodle-star",
+        "user_id" => "5",
+        "address" => "Viale dei Cipressi, 43, Rovello Porro (MI)",
+        "phone" => "3491476532",
+        "img" => "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/0c/84/97/take-sushi-ristorante.jpg?w=1200&h=-1&s=1",
+        "email" => "noodlesta@mail.it",
+        "vat_number" => "46703526973",
+        "type_id" => ""
     ],
 ];
