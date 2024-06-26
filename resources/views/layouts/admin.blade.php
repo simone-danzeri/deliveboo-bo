3553ca7945db1b1de44d55fe7a2813049defb03a
Marilisa Moio <moiomarilisa@gmail.com>
2024-06-26 22:07:07 +0200

    chore: add more style to login and dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>DeliveBoo</title>
 
     <!-- Fontawesome 6 cdn -->
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
@@ -29,11 +29,15 @@
     <div id="app">
 
 
-        <header class="navbar sticky-top flex-md-nowrap p-2 shadow justify-content-space-between">
+        <header class="navbar sticky-top flex-md-nowrap p-2 shadow ms-bg-primary justify-content-space-between">
             <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 text-white" href="/">DeliveBoo</a>
+            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 text-white" href="/">
+                <div class="logo-deliveboo">
+                    <img src="../delivebooLogo.svg" alt="" class="w-100">
+                </div>
+            </a>
             <div class="navbar-nav">
                 <div class="nav-item text-nowrap ms-2">
                     <a class="nav-link px-3 text-white" href="{{ route('logout') }}" onclick="event.preventDefault();
@@ -52,7 +56,7 @@
             <div class="row h-100">
 
 
-                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block navbar-dark sidebar collapse">
+                <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block ms-bg-primary sidebar collapse">
                     <div class="position-sticky pt-3">
                         <div class="accordion accordion-flush" id="accordionFlushExample">
                             @foreach ($user->restaurants as $rest)
