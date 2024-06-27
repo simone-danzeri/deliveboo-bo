ac015708b41be1231e3607d8a86de92ca07ed8e0
simone-danzeri <s.danzeri@icloud.com>
2024-06-27 13:30:28 +0200

    feat: add first dishes for test

--- config/dishes.php
+++ config/dishes.php
@@ -1,41 +1,51 @@
 <?php
 return [
     //antipasti
+    [
+        'restaurant_id' => 2,
+        'dish_name' => 'Involtini Primavera',
+        'category_id' => 1,
+        'dish_photo' => 'primaverasdefrcd.jpeg',
+        'is_visible' => true,
+        'price' => 7.50,
+        'description' => 'Pasta fillo ripiena di verdure.',
+        'is_vegetarian' => true
+    ],
     //primi piatti
     [
         'restaurant_id' => 1,
-        'dish_name' => 'Spaghetti Carbonara',
+        'dish_name' => 'Spaghetti Alla Carbonara',
         'category_id' => 2,
-        'dish_photo' => 'carbonara.jpg',
+        'dish_photo' => 'cover_dish/carbonaradsjhbskmc.jpeg',
         'is_visible' => true,
-        'price' => 12.50,
-        'description' => 'Traditional Italian pasta dish with eggs, cheese, pancetta, and pepper.',
+        'price' => 8.50,
+        'description' => 'La classica pasta con uova, guanciale e pecorino.',
         'is_vegetarian' => false
     ],
     //secondi piatti
     //pizze
     [
-        'restaurant_id' => 1,
-        'dish_name' => 'Margherita Pizza',
-        'category_id' => 1,
-        'dish_photo' => 'margherita.jpg',
+        'restaurant_id' => 9,
+        'dish_name' => 'Pizza Margherita',
+        'category_id' => 4,
+        'dish_photo' => 'margheritashcbhdsid.jpeg',
         'is_visible' => true,
-        'price' => 8.00,
-        'description' => 'Classic pizza with tomato sauce, mozzarella cheese, and fresh basil.',
+        'price' => 5.00,
+        'description' => 'La più famosa pizza del mondo.',
         'is_vegetarian' => true
     ],
     //panini
     //insalata
     [
-        'restaurant_id' => 2,
-        'dish_name' => 'Caesar Salad',
-        'category_id' => 3,
-        'dish_photo' => 'caesar_salad.jpg',
+        'restaurant_id' => 6,
+        'dish_name' => 'Insalata Greca',
+        'category_id' => 6,
+        'dish_photo' => 'grecashgxeuxs.jpeg',
         'is_visible' => true,
         'price' => 7.50,
-        'description' => 'Crispy romaine lettuce with Caesar dressing, croutons, and Parmesan cheese.',
-        'is_vegetarian' => false
-    ]
+        'description' => 'Dai tempi di Alessandro Magno è la prima scelta di ogni ristorante sulla acropoli.',
+        'is_vegetarian' => true
+    ],
     //dolci
     //bevande
 ];
