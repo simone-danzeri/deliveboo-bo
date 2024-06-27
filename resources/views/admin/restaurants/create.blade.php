88bfc0180ede76eb63e1a587fd0846f2083b8392
BruneiPaulPotosiCcuno <neoxtrotoka@gmail.com>
2024-06-27 18:24:44 +0200

    fix-translation-and-delete-destroy

--- resources/views/admin/restaurants/create.blade.php
+++ resources/views/admin/restaurants/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <section>
         <div class="container">
-            <h1 class="p-3">NUOVO RISTORANTE:</h1>
+            <h1 class="p-3">NEW RESTAURANT:</h1>
            
             @if ($errors->any())
                 <div class="alert alert-danger">
@@ -18,36 +18,36 @@
             <form enctype="multipart/form-data" action="{{ route('admin.restaurants.store') }}" method="POST">
                 @csrf
                 <div class="mb-3">
-                    <label for="resaturant_name" class="form-label">Nuovo Ristorante</label>
+                    <label for="resaturant_name" class="form-label">New Restaurant</label>
                     <input type="text" class="form-control" id="restaurant_name" name="restaurant_name">
                 </div>
             
                 <div class="mb-3">
-                    <label for="address" class="form-label">Indirizzo</label>
+                    <label for="address" class="form-label">Address</label>
                     <input class="form-control" id="address" rows="5" name="address"></input>
                 </div>
                 
                 <div class="mb-3">
-                    <label for="email" class="form-label">Telefono</label>
+                    <label for="email" class="form-label">Phone</label>
                     <input type="text" class="form-control" id="phone" name="phone">
                 </div>
 
                 <div class="mb-3">
-                    <label for="vat_number" class="form-label">Vat Number</label>
+                    <label for="vat_number" class="form-label">VAT Number</label>
                     <input type="text" class="form-control" id="vat_number" name="vat_number">
                 </div>
 
                 <div class="mb-3">
-                    <label for="email" class="form-label">E mail</label>
+                    <label for="email" class="form-label">Email</label>
                     <input type="text" class="form-control" id="email" name="email">
                 </div>
             
                 <div class="mb-3">
-                    <label for="img" class="form-label">Immagine</label>
+                    <label for="img" class="form-label">Image</label>
                     <input class="form-control" type="file" name="img">
                 </div>
             
-                <button type="submit" class="btn btn-primary">SALVA</button>
+                <button type="submit" class="btn btn-primary">SAVE</button>
             </form>
             
 
