69ebeb2861d6a5178f5205b513562615f8f79028
simone-danzeri <s.danzeri@icloud.com>
2024-07-08 20:10:11 +0200

    fix: fixed validation server side for new order

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -9,14 +9,14 @@ use App\Models\Order;
 class OrderController extends Controller
 {
     public function saveData(Request $request) {
-        /* $validatedData = $request->validate([
-            'client_name'=> 'required|string|max:255',
-            'client_surname'=> 'required|string|max:255',
-            'phone'=> 'required|string|max:30',
-            'email'=> 'required|email|max:100',
-            'address'=> 'required|string|max:255',
-            'total'=> 'required|numeric|max:999,99',
-        ]); */
+        $validatedData = $request->validate([
+            'params.client_name'=> 'required|string|max:255',
+            'params.client_surname'=> 'required|string|max:255',
+            'params.phone'=> 'required|string|max:30',
+            'params.email'=> 'required|email|max:100',
+            'params.address'=> 'required|string|max:255',
+            'params.total'=> 'required|numeric|max:999,99',
+        ]);
         $newOrder = Order::create([
             'client_name'=>$request['params']['client_name'],
             'client_surname'=>$request['params']['client_surname'],
@@ -24,7 +24,7 @@ class OrderController extends Controller
             'email'=>$request['params']['email'],
             'address'=>$request['params']['address'],
             'total'=>$request['params']['total'],
-        ]); 
+        ]);
         //$newOrder->fill($validatedData);
         //$newOrder->save();
         return response()->json([
@@ -33,13 +33,13 @@ class OrderController extends Controller
         ]);
 
        /*  if($validatedData){
-           
+
         }else{
             return response()->json([
                 "success" => false,
                 "results" => 'No correct data for this order'
-            ]); 
+            ]);
         }; */
-         
+
     }
 }
