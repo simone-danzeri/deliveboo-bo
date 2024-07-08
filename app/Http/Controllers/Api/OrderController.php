31c6e9f23f40d1cb0a26ecc8558529df06aab304
gianlucafalaschi <gian100gian@outlook.it>
2024-07-08 19:49:07 +0200

    order controller fixed ( partial)

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -9,28 +9,37 @@ use App\Models\Order;
 class OrderController extends Controller
 {
     public function saveData(Request $request) {
-        $validatedData = $request->validate([
+        /* $validatedData = $request->validate([
             'client_name'=> 'required|string|max:255',
             'client_surname'=> 'required|string|max:255',
             'phone'=> 'required|string|max:30',
             'email'=> 'required|email|max:100',
             'address'=> 'required|string|max:255',
             'total'=> 'required|numeric|max:999,99',
-        ]);
-        $newOrder = Order::create($validatedData); 
+        ]); */
+        $newOrder = Order::create([
+            'client_name'=>$request['params']['client_name'],
+            'client_surname'=>$request['params']['client_surname'],
+            'phone'=>$request['params']['phone'],
+            'email'=>$request['params']['email'],
+            'address'=>$request['params']['address'],
+            'total'=>$request['params']['total'],
+        ]); 
         //$newOrder->fill($validatedData);
         //$newOrder->save();
-        if($validatedData){
-            return response()->json([
-                "success" => true,
-                "results" => $newOrder
-            ]);
+        return response()->json([
+            "success" => true,
+            "results" => $newOrder
+        ]);
+
+       /*  if($validatedData){
+           
         }else{
             return response()->json([
                 "success" => false,
                 "results" => 'No correct data for this order'
-            ]);
-        };
+            ]); 
+        }; */
          
     }
 }
